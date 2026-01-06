3488e199b0873b7ea045fe8ac6e1326137babc26
Tilak-csd <tilakgubhaju3@gmail.com>
2026-01-06 23:05:30 +0545

    add

--- frontend/src/components/common/BlogSection.jsx
+++ frontend/src/components/common/BlogSection.jsx
@@ -5,7 +5,7 @@ import { blogs } from '../../data/Home';
 const BlogSection = ({title=""}) => {
     return <section className="bg-slate-50 py-20 px-6 font-poppins">
         <div className='text-center'>
-            <h1 className="text-4xl font-serif md:text-5xl font-extrabold text-[#D32F2F] tracking-tight uppercase">
+            <h1 className="text-2xl md:text-4xl font-serif font-extrabold text-[#D32F2F] tracking-tight uppercase">
                 {title}</h1>
         </div>
          <div className="flex justify-center items-center font-poppins flex-wrap gap-6 mt-8">
