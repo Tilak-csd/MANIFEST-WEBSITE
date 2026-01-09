eb60fa4c924a97449b9cd554144b52435cf0930e
Tilak-csd <tilakgubhaju3@gmail.com>
2026-01-09 13:09:00 +0545

    change the formate

--- backend/src/routes/contact.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import ContactFormController from "../controllers/contact.controllers";
-
-const route = Router()
-
-route.post('/contactForm', ContactFormController)
-
-export default route
